ba6f1e2d819a88db0c6542855037c555fa50bb01
Dil <suprads@mail.uc.edu>
2023-02-15 13:35:44 -0500

    Commit 2

--- src/TheaterKiosk.java
+++ src/TheaterKiosk.java
@@ -1,7 +1,31 @@
+import java.util.Scanner;
+
 public class TheaterKiosk
 {
     public static void main(String[] args)
     {
+        Scanner in = new Scanner(System.in);
+        int age = 0;
+        String trash = "";
 
+        System.out.print("Enter your age: ");
+
+        if (in.hasNextInt())
+        {
+            age = in.nextInt();
+            in.nextLine();
+        }
+
+        else
+        {
+            trash = in.nextLine();
+            System.out.println("You entered an incorrect value: " + trash);
+            System.exit(0);
+        }
+
+        if (age >= 21)
+        {
+            System.out.println("You get a wristband!");
+        }
     }
 }
